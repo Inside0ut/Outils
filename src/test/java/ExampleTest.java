1aa6cabfff259e8e985ed10313db6cfd21278887
Asmaa EL FRAIHI <ha_elfraihi@esi.dz>
2021-03-29 13:05:13 +0100

    Update ExampleTest.java

--- src/test/java/ExampleTest.java
+++ src/test/java/ExampleTest.java
@@ -1,7 +1,6 @@
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
-//adding a comment for test purposes 
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "Features",plugin={"json:reports/example-report.json"}
 )
